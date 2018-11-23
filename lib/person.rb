3c458114f038f330b15546146365e67067eade66
jodyneckles <jody@technative.tv>
2018-11-23 13:58:01 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -58,7 +58,8 @@ class Person
 
   def start_conversation(friend, topic)
     if topic == "politics"
-      self.happiness -= 2 && friend.happiness -= 2
+      self.happiness -= 2
+      friend.happiness -= 2
       return "blah blah partisan blah lobbyist"
     elsif topic == "weather"
       self.happiness += 1
