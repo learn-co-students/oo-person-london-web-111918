babfe1a938e29ea1d3ef79971fd37443dd9f7897
kevmsutton <kevmsutton@gmail.com>
2018-11-26 08:51:13 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,15 @@
-# your code goes here
+class Person
+
+attr_accessor :bank_account, :happiness
+attr_reader :name
+
+@@name = name
+
+def initialize(name)
+  @@name
+  @bank_account = 25
+  @happiness = 8
+end
+
+
+end
